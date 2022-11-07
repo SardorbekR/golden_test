0a11411c33e662af6cb330102bb1252229c87ebf
SardorbekR <rakhimov.sardorbek1@gmail.com>
2022-11-07 13:32:41 +0500

    Update golden_widget_test.dart

--- test/golden_widget_test.dart
+++ test/golden_widget_test.dart
@@ -1,13 +1,11 @@
 import 'dart:ui';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_test/main.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   testGoldens('Golden test', (WidgetTester tester) async {
     await loadAppFonts();
-    // await _loadPageForGolden(tester);
     await multiScreenGolden(
       tester,
       "main",
