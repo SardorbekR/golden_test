665ff75b1ac2dcc243252e5117c119696391e140
SardorbekR <rakhimov.sardorbek1@gmail.com>
2022-11-07 13:21:03 +0500

    test goldens

--- test/golden_widget_test.dart
+++ test/golden_widget_test.dart
@@ -1,9 +1,25 @@
+import 'dart:ui';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_test/main.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
-  testWidgets('Golden test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    await expectLater(find.byType(MyApp), matchesGoldenFile('main.png'));
+  testGoldens('Golden test', (WidgetTester tester) async {
+    await loadAppFonts();
+    // await _loadPageForGolden(tester);
+    await multiScreenGolden(
+      tester,
+      "main",
+      devices: [
+        Device.iphone11,
+        Device.phone.copyWith(name: "smallPhone"),
+        const Device(
+          name: "custom_device",
+          devicePixelRatio: 2,
+          size: Size(540, 960),
+        ),
+      ],
+    );
   });
 }
